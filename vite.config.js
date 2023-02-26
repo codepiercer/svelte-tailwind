5f552d95a1d648bcbe7824878850854294841ed9
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-02-25 21:05:08 -0500

    Add docker compose for dev and adminer access

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ const config = {
   define: {
     'import.meta.env.BUILD_TIME': JSON.stringify(new Date().toISOString()),
     'import.meta.env.BUILD_VERSION': JSON.stringify(process.env.npm_package_version)
+  },
+  server: {
+    port: process.env.PORT
   }
 }
 
