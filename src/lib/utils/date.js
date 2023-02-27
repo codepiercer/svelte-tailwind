fe1e0bc65687576026dcc49f4d93bf98c6cf2ad7
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2023-02-27 11:15:17 -0500

    Use standard date formatting

--- src/lib/utils/date.js
+++ src/lib/utils/date.js
@@ -1,35 +1,30 @@
-export const formatDate = (date) => {
-  // Format date to YYYY-MM-DD
-  const [dateOnly] = date.split(`T`)
-  let d = new Date(dateOnly + `T00:00:00.000`)
-  if (date.endsWith(`Z`)) {
-    d = new Date(date)
-  }
-  const year = d.getFullYear()
-  const month = `0${d.getMonth() + 1}`.slice(-2)
-  const _date = `0${d.getDate()}`.slice(-2)
-  return `${year}-${month}-${_date}`
+export const formatDate = (dateTime) => {
+  // Format to YYYY-MM-DD
+  const dateTimeObject = new Date(dateTime)
+  const year = dateTimeObject.getFullYear()
+  const month = `0${dateTimeObject.getMonth() + 1}`.slice(-2)
+  const date = `0${dateTimeObject.getDate()}`.slice(-2)
+  return `${year}-${month}-${date}`
 }
 
-export const formatTime = (date) => {
-  // if date is not a date object, convert it to one first
-  if (typeof date === `string`) {
+export const formatTime = (dateTime) => {
+  // if dateTime is not a date object, convert it to one first
+  if (typeof dateTime === `string`) {
     // covert a time only string to a date object: HH:MM -> 1970-01-01THH:MM
-    if (date.length >= 5) {
-      date = `1970-01-01T${date}`
+    if (dateTime.length >= 5) {
+      dateTime = `1970-01-01T${dateTime}`
     }
   }
-  // Format date to H:MM AM/PM
-  const d = new Date(date)
-  const hours = d.getHours()
-  const minutes = `0${d.getMinutes()}`.slice(-2)
+  // Format to H:MM AM/PM
+  const dateTimeObject = new Date(dateTime)
+  const hours = dateTimeObject.getHours()
+  const minutes = `0${dateTimeObject.getMinutes()}`.slice(-2)
   const ampm = hours >= 12 ? `PM` : `AM`
   const hours12 = hours % 12 || 12
   return `${hours12}:${minutes} ${ampm}`
 }
 
-export const formatDateTime = (date) => {
-  // Format date to YYYY-MM-DD, HH:MM AM/PM
-  const [d, t] = date.split(`T`)
-  return `${formatDate(d)}, ${formatTime(t)}`
+export const formatDateTime = (dateTime) => {
+  // Format to YYYY-MM-DD, HH:MM AM/PM
+  return `${formatDate(dateTime)}, ${formatTime(dateTime)}`
 }
